c819f833b92738360829599b79e7dc43e29fa1a2
eduskm <eduard.mihaiu@student.unitbv.ro>
2025-05-09 03:41:03 +0300

    Revert "actualizare ui (pagina pregatire pentru backend)"
    This reverts commit f3e3b11f0c096c43fad562291307ba01905ad64f.

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,10 @@
 from flask import Flask
-from dotenv import load_dotenv
-import os
-import requests
 
 app = Flask(__name__)
 
-load_dotenv()
-spoon_apikey = os.getenv("SPOONACULAR_API_KEY")
-
-@app.route("/generate-recipe")
+@app.route("/")
 def home():
-    response = requests.get(f"https://api.spoonacular.com/recipes/findByIngredients?ingredients=chicken,tomatoes,onions&number=5&apiKey={spoon_apikey}")
-    data = response.json()
-    return data
+    return {"message": "Hello from Flask!"}
 
 if __name__ == "__main__":
     app.run(debug=True)
