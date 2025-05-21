491fbca7ad51dfb7d7be4b7ef171fde91b7770f0
eduskm <eduard.mihaiu@student.unitbv.ro>
2025-05-21 17:01:08 +0300

    left bar ui finished

--- frontend/src/RecipeFinder.jsx
+++ frontend/src/RecipeFinder.jsx
@@ -212,7 +212,7 @@ function RecipeFinder() {
                                         className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition duration-300"
                                     >
                                         <h3 className="text-xl font-semibold text-gray-800 mb-2">
-                                            {recipe.sourceName}
+                                            {recipe.title}
                                         </h3>
                                         <p className="text-gray-600">
                                             <strong>Ingredients:</strong> {recipe.readyInMinutes}
